8f4fed5b1110f31019b0a4e0a3f12335e3fe2134
Brijesh Pujara <pujara.brijesh1@gmail.com>
2022-04-05 19:01:58 +0100

    Portable text and blog information created

--- sanity_medium/pages/post/[slug].tsx
+++ sanity_medium/pages/post/[slug].tsx
@@ -1,42 +1,85 @@
-import { GetStaticPaths, GetStaticProps } from "next";
-import Header from "../../components/Header"
-import { sanityClient } from "../../sanity";
-import { Post } from "../../typings";
+import { url } from 'inspector'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import PortableText from 'react-portable-text'
+import Header from '../../components/Header'
+import { sanityClient, urlFor } from '../../sanity'
+import { Post } from '../../typings'
 
 interface Props {
-    post: Post;
+  post: Post
 }
 
 function Post({ post }: Props) {
   return (
-      <main>
-          <Header />
+    <main>
+      <Header />
+
+      <img
+        className="h-48 w-full object-contain"
+        src={urlFor(post.mainImage).url()}
+        alt=""
+      />
+      <article className="mx-auto max-w-3xl p-5">
+        <h1 className="mt-10 mb-3 text-3xl">{post.title}</h1>
+              <h2 className="text-xl font-light text-gray-500">{post.description}</h2>
+              
+              <div className='flex items-center space-x-2'>
+                  <img className='h-10 w-10 rounded-full' src={urlFor(post.author.image).url()!} alt="" />
+                  <p className='font-extralight text-sm'>Blog post by <span className='text-green-600'>{post.author.name}</span> - Published at {" "}
+                      {new Date (post._createdAt).toLocaleString() }</p>
+              </div>
+
+              <div className='mt-10'>
+                  <PortableText
+                      dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
+                      projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!}
+                      content={post.body}
+                      serializers={{
+                          h1: (props: any) => (
+                              <h1 className='text-xl font-bold my-5'{...props}/>
+                          ),
+                          h2: (props: any) => (
+                            <h1 className='text-xl font-bold my-5'{...props}/>
+                          ),
+                          li: (children: any) => (
+                              <li className='ml-4 list-disc'>{children }</li>
+                          ),
+                          link: ({ href, children }: any) => (
+                              <a href={href} className='text-blue-500 hover:underline'>{children }</a>
+                          )
+                      }
+                           
+                      }
+                  />
+              </div>
+
+
+      </article>
     </main>
   )
 }
 
 export default Post
 
-export const getStaticPaths = async() => {
-    const query = `*[_type == "post"]{
+export const getStaticPaths = async () => {
+  const query = `*[_type == "post"]{
         _id,
         slug {
         current
       }
-      }`;
-    const posts = await sanityClient.fetch(query);
+      }`
+  const posts = await sanityClient.fetch(query)
 
-    const paths = posts.map((post: Post) => ({
-        params: {
-            slug: post.slug.current
-        }
-    }))
+  const paths = posts.map((post: Post) => ({
+    params: {
+      slug: post.slug.current,
+    },
+  }))
 
-    return {
-        paths,
-        fallback: 'blocking',
-    }
-
+  return {
+    paths,
+    fallback: 'blocking',
+  }
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
@@ -52,21 +95,22 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   mainImage,
   slug, 
   body
-  }`  
-    
-    const post = await sanityClient.fetch(query, {
-        slug: params?.slug,
-    });
+  }`
 
-    if (!post) {
-        return {
-            notFound: true
-        }
-    }
+  const post = await sanityClient.fetch(query, {
+    slug: params?.slug,
+  })
 
+  if (!post) {
     return {
-        props: {
-            post,
-        }
+      notFound: true,
     }
+  }
+
+  return {
+    props: {
+      post,
+    },
+    revalidate: 60, //after 60 seconds it will update the old cache version
+  }
 }
